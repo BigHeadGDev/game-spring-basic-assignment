668372d658ccb4de063da9d3ea681858321540d6
BigHeadGDev <goalkeeper0099@gmail.com>
2026-09-11 19:27:36 +0900

    Lv8 과제 완료

---
+++ src/main/java/com/gamebasic/game/dto/RenameRequest.java
@@ -0,0 +1,11 @@
+package com.gamebasic.game.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class RenameRequest
+{
+    @NotBlank
+    private String playerName;
+}
